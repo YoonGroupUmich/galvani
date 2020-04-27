2caa31981bb23929f22e5217897fbdcff69b1fc1
wangqr <wangqr@wangqr.tk>
2020-04-27 19:17:32 -0400

    Update make_dist

--- make_dist.bat
+++ make_dist.bat
@@ -1,3 +1,6 @@
-python -m PyInstaller oscgui.py --add-data "config.ini;."
+copy libgalvani\Release\libgalvani-ni.dll
+python galvani_cffi_build.py
+
+python -m PyInstaller galgui.py --add-data "config.ini;."
 
 pause
